e9b5a21c430395c3ff4836696414bf279eb99fed
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-27 18:42:21 +0300

    added score logic

--- javascript/DOM and Events/Guessing Game/script.js
+++ javascript/DOM and Events/Guessing Game/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
 document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
@@ -13,7 +14,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
     document.querySelector('.message').textContent = 'Too High';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else if (guess < secretNumber) {
     document.querySelector('.message').textContent = 'Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
 });
