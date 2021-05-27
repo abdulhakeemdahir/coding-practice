937c43bd961657894a46997797df33c664357f16
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-27 14:45:53 +0300

    Added correct, too high and too low logic

--- javascript/DOM and Events/Guessing Game/script.js
+++ javascript/DOM and Events/Guessing Game/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
-const number = Math.trunc(Math.random() * 20) + 1;
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.number').textContent = secretNumber;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -8,7 +9,11 @@ document.querySelector('.check').addEventListener('click', function () {
 
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent = 'Correct Number!';
+  } else if (guess > secretNumber) {
+    document.querySelector('.message').textContent = 'Too High';
+  } else if (guess < secretNumber) {
+    document.querySelector('.message').textContent = 'Too Low';
   }
 });
-
-// Test git add
