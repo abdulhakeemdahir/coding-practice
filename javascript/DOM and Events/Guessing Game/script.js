0faaf966cab374eb5e5fe262165f8d5317fe56d3
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-28 17:53:48 +0300

    added game over logic

--- javascript/DOM and Events/Guessing Game/script.js
+++ javascript/DOM and Events/Guessing Game/script.js
@@ -13,12 +13,20 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
   } else if (guess > secretNumber) {
-    document.querySelector('.message').textContent = 'Too High';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+    }
   } else if (guess < secretNumber) {
-    document.querySelector('.message').textContent = 'Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = 'Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'Game over!';
+    }
   }
 });
