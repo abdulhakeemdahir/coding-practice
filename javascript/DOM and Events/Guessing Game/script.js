b34735bb0a7bdd981a0ec7fbae543a3560e77f4d
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-28 18:00:04 +0300

    added background success color

--- javascript/DOM and Events/Guessing Game/script.js
+++ javascript/DOM and Events/Guessing Game/script.js
@@ -7,11 +7,14 @@ document.querySelector('.number').textContent = secretNumber;
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
-
+  // When guess is not a number
   if (!guess) {
     document.querySelector('.message').textContent = 'No Number!';
+    // When guess is correct
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent = 'Correct Number!';
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    document.querySelector('.number').style.width = '30rem';
   } else if (guess > secretNumber) {
     if (score > 1) {
       document.querySelector('.message').textContent = 'Too High';
