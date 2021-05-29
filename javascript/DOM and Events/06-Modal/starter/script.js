eca2e806b2696128bbc035748e421de6149267a0
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-29 18:00:01 +0300

    refactored open modal func

--- javascript/DOM and Events/06-Modal/starter/script.js
+++ javascript/DOM and Events/06-Modal/starter/script.js
@@ -4,17 +4,21 @@ const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtn = document.querySelectorAll('.show-modal');
 
+// Close Modal functionality
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+// Loops through button and initiates action
 for (let i = 0; i < showModalBtn.length; i++) {
-  showModalBtn[i].addEventListener('click', function () {
-    console.log('Button Clicked ' + (i + 1));
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
+  showModalBtn[i].addEventListener('click', openModal);
 }
-
+// Close Modal functionality
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+
+// Call close modal on click
 closeModalBtn.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
