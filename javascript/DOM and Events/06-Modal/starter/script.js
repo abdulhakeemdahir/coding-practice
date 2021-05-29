d01e03705fe8a51761a16773f7020228c342f6eb
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-29 17:40:42 +0300

    added button selection functionality

--- javascript/DOM and Events/06-Modal/starter/script.js
+++ javascript/DOM and Events/06-Modal/starter/script.js
@@ -3,3 +3,9 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const closeModalBtn = document.querySelector('.close-modal');
 const showModalBtn = document.querySelectorAll('.show-modal');
+
+for (let i = 0; i < showModalBtn.length; i++) {
+  showModalBtn[i].addEventListener('click', function () {
+    console.log('Button Clicked ' + (i + 1));
+  });
+}
