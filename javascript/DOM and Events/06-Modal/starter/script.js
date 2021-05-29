793e666d5df689ea134c03899d10ecc6293f3f90
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-29 17:48:33 +0300

    added close modal functionality

--- javascript/DOM and Events/06-Modal/starter/script.js
+++ javascript/DOM and Events/06-Modal/starter/script.js
@@ -7,5 +7,12 @@ const showModalBtn = document.querySelectorAll('.show-modal');
 for (let i = 0; i < showModalBtn.length; i++) {
   showModalBtn[i].addEventListener('click', function () {
     console.log('Button Clicked ' + (i + 1));
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
   });
 }
+
+closeModalBtn.addEventListener('click', function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+});
