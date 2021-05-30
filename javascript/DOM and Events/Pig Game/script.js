2ef677217537a753c96654b78a025f363e602fb4
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-30 15:47:13 +0300

    added current score tallying

--- javascript/DOM and Events/Pig Game/script.js
+++ javascript/DOM and Events/Pig Game/script.js
@@ -1,5 +1,6 @@
 'use strict';
-
+const playerOne = document.querySelector('.player--0');
+const playerTwo = document.querySelector('.player--1');
 const playerOneScore = document.querySelector('#score--0');
 const playerTwoScore = document.querySelector('#score--1');
 const playerOneCurrentScore = document.querySelector('#current--0');
@@ -11,14 +12,23 @@ playerOneCurrentScore.textContent = 0;
 playerTwoCurrentScore.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
+
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
 btnRoll.addEventListener('click', function () {
-  const dice = Math.trunc(Math.random() * 6) + 1;
+  const dice = Number(Math.trunc(Math.random() * 6) + 1);
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${dice}.png`;
+  if (dice !== 1) {
+    currentScore += dice;
+    playerOneCurrentScore.textContent = currentScore;
+  } else {
+    playerOne.classList.remove('player--active');
+    playerTwo.classList.add('player--active');
+  }
 });
 
 console.log(rollDice());
