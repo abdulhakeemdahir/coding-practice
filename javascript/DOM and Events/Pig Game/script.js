2f47522484ba5cb0bdcb2e4a0a656c397d4d7c5b
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-30 15:10:44 +0300

    setting up players

--- javascript/DOM and Events/Pig Game/script.js
+++ javascript/DOM and Events/Pig Game/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+let playerOneScore = (document.querySelector('#score--0').textContent = 17);
+let playerTwoScore = (document.querySelector('#score--1').textContent = 17);
+let playerOneCurrentScore = (document.querySelector(
+  '#current--0'
+).textContent = 17);
+let playerTwoCurrentScore = (document.querySelector(
+  '#current--1'
+).textContent = 17);
