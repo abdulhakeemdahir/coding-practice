d8a51ecf798d0823002d96cb70d25c51f93daa77
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-30 15:21:51 +0300

    added hidden class for dice

--- javascript/DOM and Events/Pig Game/script.js
+++ javascript/DOM and Events/Pig Game/script.js
@@ -1,16 +1,23 @@
 'use strict';
 
-let playerOneScore = (document.querySelector('#score--0').textContent = 17);
-let playerTwoScore = (document.querySelector('#score--1').textContent = 17);
-let playerOneCurrentScore = (document.querySelector(
-  '#current--0'
-).textContent = 17);
-let playerTwoCurrentScore = (document.querySelector(
-  '#current--1'
-).textContent = 17);
+const playerOneScore = document.querySelector('#score--0');
+const playerTwoScore = document.querySelector('#score--1');
+const playerOneCurrentScore = document.querySelector('#current--0');
+const playerTwoCurrentScore = document.querySelector('#current--1');
+const diceEl = document.querySelector('.dice');
+playerOneScore.textContent = 0;
+playerTwoScore.textContent = 0;
+playerOneCurrentScore.textContent = 0;
+playerTwoCurrentScore.textContent = 0;
+diceEl.classList.add('.hidden');
 
 const rollDice = function () {
   return Math.trunc(Math.random() * 6) + 1;
 };
 
 console.log(rollDice());
+
+switch (rollDice) {
+  case 1:
+    document.src;
+}
