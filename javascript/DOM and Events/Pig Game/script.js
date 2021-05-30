3ff7228d55cfeb07878f4625d39fb4ae321519bc
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-05-30 20:08:14 +0300

    added hold points

--- javascript/DOM and Events/Pig Game/script.js
+++ javascript/DOM and Events/Pig Game/script.js
@@ -3,16 +3,18 @@ const playerOne = document.querySelector('.player--0');
 const playerTwo = document.querySelector('.player--1');
 const playerOneScore = document.querySelector('#score--0');
 const playerTwoScore = document.querySelector('#score--1');
-const playerOneCurrentScore = document.querySelector('#current--0');
-const playerTwoCurrentScore = document.querySelector('#current--1');
+// const playerOneCurrentScore = document.querySelector('#current--0');
+// const playerTwoCurrentScore = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 playerOneScore.textContent = 0;
 playerTwoScore.textContent = 0;
-playerOneCurrentScore.textContent = 0;
-playerTwoCurrentScore.textContent = 0;
+// playerOneCurrentScore.textContent = 0;
+// playerTwoCurrentScore.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores = [0, 0];
 let currentScore = 0;
+let activePlayer = 0;
 
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
@@ -24,11 +26,23 @@ btnRoll.addEventListener('click', function () {
   diceEl.src = `dice-${dice}.png`;
   if (dice !== 1) {
     currentScore += dice;
-    playerOneCurrentScore.textContent = currentScore;
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
   } else {
-    playerOne.classList.remove('player--active');
-    playerTwo.classList.add('player--active');
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    playerOne.classList.toggle('player--active');
+    playerTwo.classList.toggle('player--active');
+
+    // playerTwo.classList.add('player--active');
   }
 });
 
-console.log(rollDice());
+btnHold.addEventListener('click', function () {
+  scores[activePlayer] += currentScore;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    scores[activePlayer];
+});
+
+console.log('Start Game');
