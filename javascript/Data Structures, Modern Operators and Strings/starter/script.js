610f5732874800c80e9b3ebe3fedb5b00ce6efeb
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-06-02 12:15:46 +0300

    destructuring arrays practice

--- javascript/Data Structures, Modern Operators and Strings/starter/script.js
+++ javascript/Data Structures, Modern Operators and Strings/starter/script.js
@@ -7,6 +7,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function (starterIndex, mainIndex) {
+    return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -22,3 +26,37 @@ const restaurant = {
     },
   },
 };
+
+// Destructuring arrays
+const arr = [2, 3, 4];
+const a = arr[0];
+const b = arr[1];
+const c = arr[2];
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
+let [main, secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// How you would do switch items before ES6
+const temp = main;
+main = secondary;
+secondary = temp;
+
+// How you can use ES6 destructuring to switch variables
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// receive 2 return values from a function
+console.table(restaurant.order(2, 0));
+const [starterCourse, mainCourse] = restaurant.order(2, 0);
+console.table(starterCourse, mainCourse);
+
+// Nested array destructuring
+const nested = [2, 4, [5, 6]];
+const [i, , j] = nested;
+const [k, , [q, u]] = nested;
+
+// Default values
+const [p = 1, q = 1, r = 1] = [8, 9];
