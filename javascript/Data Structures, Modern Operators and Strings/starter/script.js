67a36b4556180d455ffe163f45f60577a12b3bed
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-06-17 14:21:20 +0300

    map iteration

--- javascript/Data Structures, Modern Operators and Strings/starter/script.js
+++ javascript/Data Structures, Modern Operators and Strings/starter/script.js
@@ -42,33 +42,68 @@ const restaurant = {
   },
   openingHours,
 };
+// Maps iteration
+
+const question = new Map([
+  ['question', 'What is the best programming language in the world'],
+  [1, 'C'],
+  [2, 'Java'],
+  [3, 'JavaScript'],
+  ['correct', 3],
+  [true, 'Correct 🎊'],
+  [false, 'Try Again'],
+]);
+console.log(question);
+
+//Convert object to map
+console.log(Object.entries(openingHours));
+const hoursMap = new Map(Object.entries(openingHours));
+console.log(hoursMap);
+
+//Maps are iterable and you can use For Loops
+console.log(question.get('question'));
+
+for (const [key, value] of question) {
+  if (typeof key === 'number') {
+    console.log(`Answer ${key}: ${value}`);
+  }
+}
+
+const answer = Number(prompt('Your Answer'));
+console.log(answer);
+
+console.log(question.get(answer === question.get('correct')));
+
+// Convert Map into Array
+console.log([...question]);
+
 // Maps
-const rest = new Map();
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Fiernz Italy');
-rest.set(2, 'Lisbon Portugal');
-console.log(rest);
+// const rest = new Map();
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Fiernz Italy');
+// rest.set(2, 'Lisbon Portugal');
+// console.log(rest);
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open')
-  .set(false, 'We are closed');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegeterian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open')
+//   .set(false, 'We are closed');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest);
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest);
 
-const time = 21;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 21;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
-console.log(rest.has('categories'));
+// console.log(rest.has('categories'));
 
-rest.set([1, 2], 'Test');
-rest.set(document.querySelector('h1'), 'Heading');
-console.log(rest);
-console.log(rest.get([1, 2]));
+// rest.set([1, 2], 'Test');
+// rest.set(document.querySelector('h1'), 'Heading');
+// console.log(rest);
+// console.log(rest.get([1, 2]));
 
 // Sets
 // const orderSet = new Set(['Pasta', 'Pizza', 'Risotto', 'Pasta', 'Pizza']);
