c0d5104952a618b6fff73f5cab6bed5a1d24f01d
Abdulhakeem Dahir <72118224+abdulhakeemdahir@users.noreply.github.com>
2021-06-17 15:56:13 +0300

    coding challenge 3

--- javascript/Data Structures, Modern Operators and Strings/starter/script.js
+++ javascript/Data Structures, Modern Operators and Strings/starter/script.js
@@ -44,38 +44,38 @@ const restaurant = {
 };
 // Maps iteration
 
-const question = new Map([
-  ['question', 'What is the best programming language in the world'],
-  [1, 'C'],
-  [2, 'Java'],
-  [3, 'JavaScript'],
-  ['correct', 3],
-  [true, 'Correct 🎊'],
-  [false, 'Try Again'],
-]);
-console.log(question);
+// const question = new Map([
+//   ['question', 'What is the best programming language in the world'],
+//   [1, 'C'],
+//   [2, 'Java'],
+//   [3, 'JavaScript'],
+//   ['correct', 3],
+//   [true, 'Correct 🎊'],
+//   [false, 'Try Again'],
+// ]);
+// console.log(question);
 
-//Convert object to map
-console.log(Object.entries(openingHours));
-const hoursMap = new Map(Object.entries(openingHours));
-console.log(hoursMap);
+// //Convert object to map
+// console.log(Object.entries(openingHours));
+// const hoursMap = new Map(Object.entries(openingHours));
+// console.log(hoursMap);
 
-//Maps are iterable and you can use For Loops
-console.log(question.get('question'));
+// //Maps are iterable and you can use For Loops
+// console.log(question.get('question'));
 
-for (const [key, value] of question) {
-  if (typeof key === 'number') {
-    console.log(`Answer ${key}: ${value}`);
-  }
-}
+// for (const [key, value] of question) {
+//   if (typeof key === 'number') {
+//     console.log(`Answer ${key}: ${value}`);
+//   }
+// }
 
-const answer = Number(prompt('Your Answer'));
-console.log(answer);
+// const answer = Number(prompt('Your Answer'));
+// console.log(answer);
 
-console.log(question.get(answer === question.get('correct')));
+// console.log(question.get(answer === question.get('correct')));
 
-// Convert Map into Array
-console.log([...question]);
+// // Convert Map into Array
+// console.log([...question]);
 
 // Maps
 // const rest = new Map();
